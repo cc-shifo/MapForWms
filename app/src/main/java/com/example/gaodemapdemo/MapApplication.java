6b3bfeb6cc3ccc8b9b704136511f036eedefe6e6
cc-shifo <cc-shifo>
2022-05-11 14:28:03 +0800

    利用TileProvider加载wms服务，wms数据源由高德地图的geojson数据转化成的shapefile。没有出现偏移。

---
+++ app/src/main/java/com/example/gaodemapdemo/MapApplication.java
@@ -0,0 +1,11 @@
+package com.example.gaodemapdemo;
+
+import android.app.Application;
+
+/**
+ * Created by zkj on 2017/08/11
+ * GaoDeMapDemo
+ */
+
+public class MapApplication extends Application {
+}
